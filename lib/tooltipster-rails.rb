cd7eb54d97d3704ad3bef5a5f5baecf2598741da
ldewald <laurardewald@gmail.com>
2014-05-26 22:41:30 -0700

    Restructuring files

--- lib/tooltipster-rails.rb
+++ lib/tooltipster-rails.rb
@@ -1,3 +1,2 @@
-if defined? Rails && Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 1
-  require 'tooltipster/rails'
-end
+require "tooltipster-rails/version"
+require "tooltipster-rails/engine" if defined?(::Rails)
